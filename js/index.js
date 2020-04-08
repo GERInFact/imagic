d4d91460ba6b562de747aabdbd9a6f8e8eea1102
M. Hille <marciausgoslar@gmail.com>
2020-04-08 11:07:58 +0200

    Added js comments

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@
 const $burgerCheckMark = document.querySelector(".nav_burger-check-mark");
 const $menuItems = document.querySelectorAll(".menu_item");
 
+// Toggles Menu on Mobile
 (function toggleMenuBar() {
   addLinkEvents();
 })();
 
+// Adds toggle function to all nav links
 function addLinkEvents() {
   $menuItems.forEach((i) => {
     i.addEventListener("click", (e) => {
